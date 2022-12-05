7591cb9c34021a7ae1b57890acf28901ffa85d68
louvo24 <luannvo@yahoo.com.vn>
2022-12-04 22:19:14 -0800

    Create table commands for database

---
+++ CREATE TABLE `Cars.com`(.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `Cars.com`(
+    `Order` VARCHAR(3) PRIMARY KEY, 
+    `Vehicle Name` VARCHAR(200),
+    `Price` VARCHAR(20),
+    `Dealer Name` VARCHAR(200),
+    `LINK` VARCHAR(500)
+);
+
